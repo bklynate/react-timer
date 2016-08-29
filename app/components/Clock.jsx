9834edd5312a1585ad40024032026002883610dc
bklynate <nathanielcfa@gmail.com>
2016-08-29 17:40:24 -0400

    edit fixed the clock testing

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -4,6 +4,8 @@ var Clock = React.createClass({
   formatSeconds: function(totalSeconds){
     var seconds = totalSeconds % 60;
     var minutes = Math.floor(totalSeconds / 60);
+
+    return minutes + ":" + seconds;
   },
   render: function() {
     return (
--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -1,17 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var expect = require('expect');
-var $ = require('jQuery');
+var $ = require('jquery');
 var TestUtils = require('react-addons-test-utils');
 
 var Clock = require('Clock');
 
 describe('Clock', () => {
-  it('should exist', () => {
+  // body...
+  it('should exist', ()=>{
     expect(Clock).toExist();
   });
 
   describe('formatSeconds', () => {
+    // body...
     it('should format seconds', () => {
       var clock = TestUtils.renderIntoDocument(<Clock/>);
       var seconds = 615;
@@ -20,5 +22,14 @@ describe('Clock', () => {
 
       expect(actual).toBe(expected);
     });
-  }); 
+
+    // it('should format seconds when min/sec are less than 10', () => {
+    //   var clock = TestUtils.renderIntoDocument(<Clock/>)
+    //   var seconds = 61;
+    //   var expected = '01:01';
+    //   var actual = clock.formatSeconds(seconds);
+    //
+    //   expect(actual).toBe(expected);
+    // });
+  });
 });
