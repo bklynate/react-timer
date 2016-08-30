2ce00121e1546f7f33511d2aa98a8726b5cfca0e
Nathaniel Quashie <nathanielcfa@gmail.com>
2016-08-30 18:23:14 +0000

    add countdownform component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
   render: function(){
     return(
       <div className>
         <Clock totalSeconds={129}/>
+        
+        <CountdownForm />
       </div>
     )
   }
---
+++ app/components/CountdownForm.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+
+var CountdownForm = React.createClass({
+  onFormSubmit: function(e){
+    e.preventDefault()
+  },
+  render: function(){
+    return(
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          <form onSubmit={this.onFormSubmit}>
+            <input type="search" ref="timeInSeconds" placeholder="Input the amount of time"/>
+            <input type="submit" className="expanded button hollow" value="Add Time"/>
+          </form>
+        </div>  
+      </div>
+    );
+  }
+})
+
+module.exports = CountdownForm;
