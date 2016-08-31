bfff073dbf97ecc19876c40da09e51ea005bc5d7
bklynate <nathanielcfa@gmail.com>
2016-08-30 23:32:57 -0400

    pushing from local

---
+++ app/components/CountdownForm.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var CountdownForm = () => {
+  return(
+    <div>
+      <form>
+        <input type="text"/>
+        <input type="submit"/>
+      </form>
+    </div>
+  )
+};
+
+module.exports = CountdownForm;
