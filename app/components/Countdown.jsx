7c04ab9156c2ec784781a8bfe45dd0a2066a4899
bklynate <nathanielcfa@gmail.com>
2016-08-31 16:32:22 -0400

    Error with updating clock

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,22 +4,44 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
   getInitialState: function(){
-    return {count: 0};
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    };
+  },
+  componentDidUpdate: function(prevProps, prevState){
+    if(this.state.countdownStatus !== prevState.countdownStatus){
+      switch (this.state.countdownStatus){
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function(){
+    this.timer = setInterval(function(){
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
   },
   handleAddTime: function(timeInSeconds){
     this.setState({
-      count: timeInSeconds
+      count: timeInSeconds,
+      countdownStatus: 'started'
     });
   },
   render: function(){
     var {count} = this.state;
+
     return(
-      <div className>
+      <div>
         <Clock totalSeconds={count}/>
         <CountdownForm onAddTime={this.handleAddTime}/>
       </div>
-    )
+    );
   }
-})
+});
 
 module.exports = Countdown;
--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -5,6 +5,7 @@ var CountdownForm = React.createClass({
     e.preventDefault();
 
     var timeInSeconds = this.refs.timeInSeconds.value;
+
     if(timeInSeconds.match(/^[0-9]*$/)){
       this.refs.timeInSeconds.value = "";
       this.props.onAddTime(parseInt(timeInSeconds, 10));
@@ -12,13 +13,11 @@ var CountdownForm = React.createClass({
   },
   render: function(){
     return(
-      <div className="row">
-        <div className="columns medium-6 large-4 small-centered">
-          <form onSubmit={this.onFormSubmit}>
-            <input type="search" ref="timeInSeconds" placeholder="Input the amount of time"/>
-            <button className="expanded button hollow">Add Time</button>
-          </form>
-        </div>
+      <div>
+        <form ref="form" onSubmit={this.onFormSubmit}>
+          <input type="text" ref="timeInSeconds" placeholder="Input time in seconds"/>
+          <button className="expanded button hollow">Add Time</button>
+        </form>
       </div>
     );
   }
