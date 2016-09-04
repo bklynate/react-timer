501539e57bbfe54b4e85b0736150b2afabc2789f
bklynate <nathanielcfa@gmail.com>
2016-09-04 02:05:59 -0400

    Add test suite for Timer component

---
+++ app/tests/components/Timer.test.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jquery');
+var TestUtils = require('react-addons-test-utils');
+
+var Timer = require('Timer');
+
+describe('Timer', () => {
+  // body...
+  it('should exist', () => {
+    expect(Timer).toExist();
+  });
+});
